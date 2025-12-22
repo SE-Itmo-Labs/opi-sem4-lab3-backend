e30ec8057af1058dfa40e9ee304835e7908d2f70
Stanislav Snagin <s_lstoo@bk.ru>
2025-12-22 16:34:50 +0300

    💻 make example user/auth endpoint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    war
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 group = "com.ssnagin"
@@ -10,12 +12,33 @@ repositories {
 }
 
 dependencies {
+    compileOnly("jakarta.platform:jakarta.jakartaee-web-api:10.0.0")
+    compileOnly("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
+
+    providedCompile("jakarta.platform:jakarta.jakartaee-api:10.0.0")
+    implementation("org.eclipse.persistence:org.eclipse.persistence.jpa:4.0.8")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
+
+    providedCompile("org.eclipse.persistence:eclipselink:4.0.8")
+
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(17)
+}
+
+tasks.war {
+    webAppDirectory = file("src/main/webapp")
+    archiveFileName.set("lab4.war")
+}
+
+ktlint {
+    version.set("1.3.1")
+    verbose.set(true)
 }
